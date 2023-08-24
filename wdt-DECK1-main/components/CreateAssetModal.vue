7178e6e354efe563de6b3bb1a4e3b12434b25fc9
lvanlvanov <93519832+lvanlvanov@users.noreply.github.com>
2023-08-24 18:02:46 +0200

    Merge pull request #1 from dankyyyyy/Add-WTG-as-a-New-Asset-Category-+-Assign-Attributes-(See-Excel---WTG)
    Add WTG as an Asset implemented

--- wdt-DECK1-main/components/CreateAssetModal.vue
+++ wdt-DECK1-main/components/CreateAssetModal.vue
@@ -2,7 +2,7 @@
   <div class="overlay" @click="hideModal">
     <div class="modal rounded-lg flex-col">
       <h3 class="font-semibold">Asset creation</h3>
-      <div class="py-5 flex flex-col flex-wrap content-center">
+      <div class="py-5 flex flex-col flex-wrap content-normal">
         <div
           class="border-2 border-dashed rounded-md h-56 flex flex-col justify-center items-center"
         >
@@ -17,7 +17,7 @@
           <input
             type="text"
             v-model="this.asset.id"
-            class="border-2 rounded-md text-center"
+            class="border-2 rounded-md text-left"
             disabled
           />
         </div>
@@ -30,19 +30,20 @@
           />
         </div>
         <div class="create-input">
-          <label for="name">Category: </label>
-          <select name="category" v-model="asset.category">
+          <label for="name">Category:</label>
+          <select name="category" v-model="asset.category" class="border-2 rounded-sm text-left">
             <option value="Vessel">Vessel</option>
             <option value="Helicopter">Helicopter</option>
+            <option value="WindTurbineGenerator">Wind turbine generator</option>
           </select>
         </div>
         <div v-if="asset.category === 'Vessel'">
-          <div class="create-input">
+          <div class="create-input whitespace-nowrap">
             <label for="limit">HS limit: </label>
             <input
               type="text"
               v-model="asset.hs"
-              class="border-2 rounded-md text-center"
+              class="border-2 rounded-sm text-center "
             />
           </div>
         </div>
@@ -64,6 +65,40 @@
             />
           </div>
         </div>
+        <div v-else-if="asset.category === 'WindTurbineGenerator'">
+          <div class="create-input whitespace-nowrap">
+            <label for="limit">Windspeed limit: </label>
+            <input
+              type="text"
+              v-model="asset.windspeedlimit"
+              class="border-2 rounded-sm text-center"
+            />
+          </div>
+          <div class="create-input whitespace-nowrap">
+            <label for="limit">Planned Maintenance: </label>
+            <input
+              type="text"
+              v-model="asset.plannedmaintenance"
+              class="border-2 rounded-sm text-center"
+              />
+              </div>
+        <div class="create-input whitespace-nowrap">
+            <label for="limit">Troubleshoot Visitings: </label>
+            <input
+              type="text"
+              v-model="asset.troubleshootvisitings"
+              class="border-2 rounded-sm text-center"
+              />
+              </div>
+              <div class="create-input whitespace-nowrap">
+            <label for="limit">Average TS hours: </label>
+            <input
+              type="text"
+              v-model="asset.averagetshours"
+              class="border-2 rounded-sm text-center"
+              />
+              </div>
+      </div>
       </div>
       <div class="flex w-full justify-end">
         <button
@@ -95,7 +130,6 @@ export default {
   data() {
     return {
       asset: {
-        id: "auto",
         name: "",
         category: "",
       },
--- wdt-DECK1-main/pages/assets.vue
+++ wdt-DECK1-main/pages/assets.vue
@@ -28,6 +28,17 @@
           <AssetCard :asset="asset" />
         </div>
       </div>
+
+      <h2 class="text-lg font-semibold w-full pl-5 pt-5">Wind Turbine Generators</h2>
+      <div class="flex pb-5"  v-if="assets !== undefined">
+        <div
+          v-for="asset in assets.filter((x) => x.category === 'WindTurbineGenerator')"
+          :key="asset.id"
+          class="p-5"
+        >
+          <AssetCard :asset="asset" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
